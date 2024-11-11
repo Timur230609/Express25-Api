ced3a4aa75453897675595aa0de6869352c4bdd4
Ocak <sayfullayeva191@gmail.com>
2024-11-11 15:22:32 +0500

    account validate

--- store/serializers.py
+++ store/serializers.py
@@ -12,11 +12,13 @@ class StoreSerializer(serializers.ModelSerializer):
         model = Category
         
         fields =  "__all__"
-
     def validate(self, data):
         # addressni tekshirasilar
+        address_id = data.get("address")
+        if Category.objects.filter(address_id = address_id).exists():
+            raise serializers.ValidationError('Address topilmadi')
         phone_number = data.get("phone_number")
-        if Category.objects.filter(phone_number=phone_number):
+        if Category.objects.filter(phone_number=phone_number).exists():
             raise serializers.ValidationError("Bu telefon nomerdan Dokon yaratilgan")
 
 
