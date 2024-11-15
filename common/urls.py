9f9c50bbee1f591160a02a0634dfd541907d825c
muslimbek77 <muslimjonbekali@mail.ru>
2024-11-15 17:31:45 +0500

    add address and pastic-card

--- common/urls.py
+++ common/urls.py
@@ -1,18 +1,3 @@
-from .views import AddressViewSet, PlasticCardViewSet
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 
-# Define routers
-router = DefaultRouter()
-router.register(r'addresses', AddressViewSet, basename='address')
-router.register(r'plastic-cards', PlasticCardViewSet, basename='plasticcard')
 
-# Manually add custom paths if required
-custom_urlpatterns = [
-    path('addresses/<int:pk>/', AddressViewSet.as_view({'get': 'retrieve'}), name='address-detail'),
-    path('plastic-cards/<int:pk>/', PlasticCardViewSet.as_view({'get': 'retrieve'}), name='plastic-card-detail'),
-]
 
-urlpatterns = [
-    path('', include(router.urls)),
-] + custom_urlpatterns
--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from accaunt.views import LogoutView,LoginView,RegisterView
-
+from common.views import AddressViewSet, PlasticCardViewSet
+from rest_framework.routers import DefaultRouter
 
 
 schema_view = get_schema_view(
@@ -19,12 +20,26 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
+
+
+
+
+
+# Define routers
+router = DefaultRouter()
+router.register(r'addresses', AddressViewSet, basename='address')
+router.register(r'plastic-cards', PlasticCardViewSet, basename='plasticcard')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/store/',include('store.urls'),name='store-api' ),
     path('api/v1/delivery/',include('delivery.urls')),
-    path('api/auth/users/',include("common.urls")),
+    
+    path('addresses/<int:pk>/', AddressViewSet.as_view({'get': 'retrieve'}), name='address-detail'),
+    path('plastic-cards/<int:pk>/', PlasticCardViewSet.as_view({'get': 'retrieve'}), name='plastic-card-detail'),
+
     path('api/auth/register/', RegisterView.as_view(), name='register'),
     path('api/auth/login/', LoginView.as_view(), name='login'),
     path('api/auth/logout/', LogoutView.as_view(), name='logout'),
